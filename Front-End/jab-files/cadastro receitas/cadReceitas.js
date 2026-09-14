bcc05639238fa2d3087b9d94ea0c09135bec62a8
4rauj6 <joaovas123m@gmail.com>
2026-09-14 12:51:43 -0300

    Lógica, design e responsividade foram aprimorados: JS - Lógica do modal aprimorado. Criação e renderização da tabela apos salvar a receita adicionado (Necessário aprimorar sua estilização e responsividade). HTML - Processo de limpeza da sintaxe do arquivo para melhorar a leitura e manutenção do código em futuros updates. CSS - responsividade melhorarda.

--- Front-End/jab-files/cadastro receitas/cadReceitas.js
+++ Front-End/jab-files/cadastro receitas/cadReceitas.js
@@ -2,18 +2,36 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const modalButtons = document.querySelectorAll(".addIngred, .close-modal");
+const outsideModalBody = document.querySelector(".hero");
+
+const getIngredName = document.getElementById("nomeIngred");
+const getIngredQuant = document.getElementById("quantIngred");
+const getIngredGr = document.getElementById("gramaDoIngred");
+const getINgredType = document.getElementById("tipoIngred");
+const saveButton = document.querySelector(".save-ingred");
 
 function setModalState(isOpen) {
   modal.classList.toggle("hidden", !isOpen);
   overlay.classList.toggle("hidden", !isOpen);
+
+  if (!isOpen) {
+    [getIngredName, getIngredQuant, getINgredType].forEach(
+      (index) => (index.selectedIndex = 0),
+    );
+    getIngredQuant.disabled = true;
+    getIngredGr.textContent = "";
+    getIngredGr.style.display = "none";
+  }
 }
 
 modalButtons.forEach((modalElement) => {
   modalElement.addEventListener("click", () => {
     if (modalElement.classList.contains("addIngred")) {
       setModalState(true);
+      outsideModalBody.style.filter = "grayscale(100%)";
     } else {
       setModalState(false);
+      outsideModalBody.style.filter = "grayscale(0)";
     }
   });
 });
@@ -83,4 +101,60 @@ percentIngred.addEventListener("change", () => {
 
   resultPlacement.innerText = `${result.toFixed(2)} g`;
   resultPlacement.style.backgroundColor = "#f2e8d9";
+});
+
+/*LÓGICA DE CRIAR E RENDERIZAR A TABELA PELO MODAL DE CADASTRAR RECEITA*/
+const tablePlacement = document.getElementById("tablePlace");
+
+saveButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const name = getIngredName.options[getIngredName.selectedIndex]?.text || "";
+  const quant =
+    getIngredQuant.options[getIngredQuant.selectedIndex]?.text || "";
+  const grama = getIngredGr.textContent || "0g";
+  const type = getINgredType.options[getINgredType.selectedIndex]?.text || "";
+
+  if (!getIngredName.value || !getIngredQuant.value) {
+    alert("Todos os campos precisão ser preenchidos antes de salvar");
+    return;
+  }
+
+  let table = document.createElement("table");
+  let th = document.createElement("thead");
+  let tr = document.createElement("tr");
+  let tb = tablePlacement.querySelector("tbody");
+
+  if (!tb) {
+    tb = document.createElement("tbody");
+
+    th.innerHTML = `
+    <tr>
+      <th>Nome do ingrediente</th>
+      <th>Porcentagem</th>
+      <th>Quantidade em grama</th>
+      <th>Tipo</th>
+    </tr>`;
+
+    table.appendChild(th);
+    table.appendChild(tb);
+    tablePlacement.appendChild(table);
+  }
+
+  tr.innerHTML = `
+      <td>${name}</td>
+      <td>${quant}</td>
+      <td>${grama}</td>
+      <td>${type}</td>
+  `;
+
+  tb.appendChild(tr);
+
+  getIngredName.selectedIndex = 0;
+  getIngredQuant.selectedIndex = 0;
+  getIngredQuant.disabled = true;
+  getIngredGr.textContent = "";
+  getINgredType.selectedIndex = 0;
+
+  setModalState(false);
 });
